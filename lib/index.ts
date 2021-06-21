55447c18157f43e3679419a6aade71560bd3b106
Yahia KERIM <yahiakerim@gmail.com>
2021-06-21 12:00:29 +0100

    refactor(packages): delete @types/mongoose

--- lib/index.ts
+++ lib/index.ts
@@ -1,14 +1,8 @@
-import { Document, Schema, HookNextFunction, SchemaType } from 'mongoose'
+import { Document, Schema, HookNextFunction } from 'mongoose'
 
 export default function mongoosastic(schema: Schema, options: any): void {
 
 	schema.post(['find', 'findOne'], (docs: Document[], next: HookNextFunction) => {
-		
-		schema.eachPath((path: string, type: any ) => {
-			console.log(`Path: ${path}, type: ${type.instance}, embedded: ${type.$embeddedSchemaType ?
-				type.$embeddedSchemaType.instance: false}`)
-		})
-		
 		console.log('Found those documents:', docs)
 		next()
 	})
