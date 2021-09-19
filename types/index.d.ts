549089355e0de73ef77ab2df1e0e96ce69c31f25
Yahia KERIM <yahiakerim@gmail.com>
2021-09-19 12:09:28 +0100

    feat(types): define model static methods

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,12 +1,47 @@
-import { ClientOptions } from '@elastic/elasticsearch'
-import { Aggregation, Highlight, Suggest } from '@elastic/elasticsearch/api/types'
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { ClientOptions, ApiResponse } from '@elastic/elasticsearch'
+import { Aggregation, Highlight, Suggest, BulkResponse, CountResponse, RefreshResponse, SearchResponse } from '@elastic/elasticsearch/api/types'
+import { EventEmitter } from 'events'
+import { Schema } from 'mongoose'
 import { Document, Model, PopulateOptions, QueryOptions } from 'mongoose'
+
+declare interface FilterFn {
+    (doc: Document): boolean;
+}
+declare interface TransformFn {
+    (doc: Document): boolean;
+}
+declare interface CreateMappingCallbackFn {
+    (err: any | null | undefined, inputMapping: any | null | undefined): void;
+}
+declare interface TruncateCallbackFn {
+    (err: any | null | undefined): void;
+}
+declare interface SearchCallbackFn<T> {
+    (err: null | undefined, resp: ApiResponse<SearchResponse<T>>): void;
+    (err: any, resp: null | undefined): void;
+}
+declare interface CountCallbackFn {
+    (err: null | undefined, resp: ApiResponse<CountResponse>): void;
+    (err: any, resp: null | undefined): void;
+}
+declare interface FlushCallbackFn {
+    (err: null | undefined, resp: ApiResponse<BulkResponse>): void;
+    (err: any, resp: null | undefined): void;
+}
+declare interface RefreshCallbackFn {
+    (err: null | undefined, resp: ApiResponse<RefreshResponse>): void;
+    (err: any, resp: null | undefined): void;
+}
 
 declare class PluginDocument extends Document {
 	index(cb?: CallableFunction): void
 	unIndex(cb?: CallableFunction): void
 	emit(event: string, ...args: any): void
 	esOptions(): Options
+	on(event: string, cb?: CallableFunction): void
+	once(event: string, cb?: CallableFunction): void
 }
 
 declare type Options = {
@@ -38,6 +73,22 @@ declare type EsSearchOptions = {
     hydrate?: boolean,
     hydrateOptions?: QueryOptions,
     hydrateWithESResults?: any
+}
+
+declare module 'mongoosastic' {
+    const Mongoosastic: (schema: Schema, Options?: Partial<Options>) => void
+    export = Mongoosastic;
+}
+  
+declare module 'mongoose' {
+    export function model<T extends PluginDocument>(name: string, schema?: Schema<T>, collection?: string, skipInit?: boolean): MongoosasticModel<T>;
+    export interface MongoosasticModel<T extends Document> extends Model<T> {
+        search(query: any, options: EsSearchOptions, cb?: SearchCallbackFn<T>): void;
+        esSearch(query: any, options: EsSearchOptions, cb?: SearchCallbackFn<T>): void;
+        synchronize(query?: any, options?: any): EventEmitter;
+        esOptions(): Options
+        createMapping(body: any, cb: SearchCallbackFn<T>): void
+    }
 }
 
 export {
