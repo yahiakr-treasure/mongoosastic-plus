c986f825a28774dc0b6a6ab3176849425c68aa35
Yahia KERIM <yahiakerim@gmail.com>
2021-09-19 14:50:24 +0100

    test(bulk): refactor config methods from callback-based to promise-based

--- tests/bulk.test.ts
+++ tests/bulk.test.ts
@@ -1,7 +1,6 @@
 'use strict'
 
 import mongoose from 'mongoose'
-import async from 'async'
 import { config } from './config'
 const Schema = mongoose.Schema
 import mongoosastic from '../lib/index'
@@ -20,55 +19,46 @@ BookSchema.plugin(mongoosastic, {
 const Book = mongoose.model('Book', BookSchema)
 
 describe('Bulk mode', function () {
-	beforeAll(function (done) {
-		config.deleteIndexIfExists(['books'], function () {
-			mongoose.connect(config.mongoUrl, config.mongoOpts, function () {
-				const client = mongoose.connections[0].db
-				client.collection('books', function () {
-					Book.deleteMany(done)
-				})
+
+	beforeAll(async function (done) {
+		await config.deleteIndexIfExists(['books'])
+		mongoose.connect(config.mongoUrl, config.mongoOpts, function () {
+			const client = mongoose.connections[0].db
+			client.collection('books', function () {
+				Book.deleteMany(done)
 			})
 		})
 	})
 
-	beforeAll(function (done) {
-		async.forEach(config.bookTitlesArray(), function (title, cb) {
-			new Book({
-				title: title
-			}).save(cb)
-		}, done)
+	beforeAll(async function () {
+		for (const title of config.bookTitlesArray()) {
+			await new Book({ title: title }).save()
+		}
 	})
 
-	beforeAll(function (done) {
-		Book.findOne({
-			title: 'American Gods'
-		}, function (err: any, book: any) {			
-			book.remove(done)
-		})
+	beforeAll(async function () {
+		const book = await Book.findOne({ title: 'American Gods' })
+		await book?.remove()
 	})
 
-	afterAll(function (done) {
-		config.deleteIndexIfExists(['books'], function () {
-			Book.deleteMany(function () {
-				mongoose.disconnect()
-				done()
-			})
-		})
+	afterAll(async function () {
+		await config.deleteIndexIfExists(['books'])
+		await Book.deleteMany()
+		mongoose.disconnect()
 	})
 
 	it('should index all objects and support deletions too', function (done) {
 		// This timeout is important, as Elasticsearch is "near-realtime" and the index/deletion takes time that
 		// needs to be taken into account in these tests
 		setTimeout(function () {
-			(Book as any).search({
+			Book.search({
 				match_all: {}
-			}, {}, function (err: any, results: any) {
-				// results.should.have.property('hits').with.property('total', 52)
+			}, {}, function (err, results) {
 				expect(results).toHaveProperty('body')
-				expect(results.body).toHaveProperty('hits')
-				expect(results.body.hits).toHaveProperty('total', 52)
+				expect(results?.body).toHaveProperty('hits')
+				expect(results?.body.hits).toHaveProperty('total', 52)
 				done()
 			})
-		}, config.BULK_ACTION_TIMEOUT as number)
+		}, config.BULK_ACTION_TIMEOUT)
 	})
 })
