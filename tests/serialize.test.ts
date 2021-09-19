711923a4cdf57052007414aeb58ad1f9c35cb033
Yahia KERIM <yahiakerim@gmail.com>
2021-09-19 15:05:10 +0100

    test(serialize): refactor the callback-based methods

--- tests/serialize.test.ts
+++ tests/serialize.test.ts
@@ -48,7 +48,7 @@ describe('serialize', function () {
 			date: new Date(Date.parse('06/17/1962'))
 		}],
 		somethingToCast: 'Something'
-	})
+	}) as any
 
 	// another person with missing parts to test robustness
 	const millionnaire = new Person({
@@ -56,44 +56,36 @@ describe('serialize', function () {
 			first: 'Jeffrey',
 			last: 'Lebowski'
 		}
-	}).toObject()
+	})
 
 	it('should serialize a document with missing bits', function () {
 		const serialized = serialize(millionnaire, mapping)
-		// serialized.should.have.property('games', [])
-		expect(serialized).toHaveProperty('games', [])
+		expect(serialized).toHaveProperty('games')
+		expect(serialized.games).toHaveLength(0)
 	})
 
 	describe('with no indexed fields', function () {
 		const serialized = serialize(dude, mapping)
 		it('should serialize model fields', function () {
-			// serialized.name.first.should.eql('Jeffrey')
 			expect(serialized.name.first).toEqual('Jeffrey')
-			// serialized.name.last.should.eql('Lebowski')
 			expect(serialized.name.last).toEqual('Lebowski')
 		})
 
 		it('should serialize object ids as strings', function () {
-			// serialized.bowlingBall.should.eql(dude.bowlingBall)
 			expect(serialized.bowlingBall).toEqual(dude.bowlingBall)
-			// serialized.bowlingBall.should.be.type('object')
 			expect(typeof serialized.bowlingBall).toBe('object')
 		})
 
 		it('should serialize dates in ISO 8601 format', function () {
-			// serialized.dob.should.eql(dude.dob.toJSON())
 			expect(serialized.dob).toEqual(dude.dob.toJSON())
 		})
 
 		it('should serialize nested arrays', function () {
-			// serialized.games.should.have.lengthOf(2)
 			expect(serialized.games).toHaveLength(2)
-			// serialized.games[0].should.have.property('score', 80)
 			expect(serialized.games[0]).toHaveProperty('score', 80)
 		})
 
 		it('should cast and serialize field', function () {
-			// serialized.somethingToCast.should.eql('Something has been cast')
 			expect(serialized.somethingToCast).toEqual('Something has been cast')
 		})
 	})
